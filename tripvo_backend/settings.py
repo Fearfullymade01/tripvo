75228fba6b36d8623bcdf5718d9fa4b20160bc6f
fearfullymade01 <gegbeyemi.poise@gmail.com>
2026-02-19 16:34:57 +0000

    Move corsheaders.middleware.CorsMiddleware to top of MIDDLEWARE list for proper CORS handling

--- tripvo_backend/settings.py
+++ tripvo_backend/settings.py
@@ -95,9 +95,9 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
